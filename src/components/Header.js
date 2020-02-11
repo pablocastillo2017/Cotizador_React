de2cc1ae7d464e6683e184f67c5682d5373d82c7
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-02-11 13:29:36 -0300

    7-87 Agregando Emotion Styled Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import styled from '@emotion/styled';
+
+// link Documentacion
+// https://emotion.sh/docs/introduction
+
+
+const ContenedorHeader = styled.header`
+ 
+  background-color: #26C6DA;
+  padding:10px;
+  font-weight:bold;
+  color:#FFFFFF;
+
+`
+// para Centrar El texto
+const TextoHeader = styled.h1`
+    font-size:2rem;
+    margin:0;
+    font-family: 'Slabo 27px',serif;
+    text-align: center;
+`
 
 const Header = ({titulo}) => {
     return ( 
 
-        <header>
-            <h1>{titulo}</h1>
-        </header>
+        <ContenedorHeader>
+            <TextoHeader>{titulo}</TextoHeader>
+        </ContenedorHeader>
         
      );
+
 }
  
 export default Header;
