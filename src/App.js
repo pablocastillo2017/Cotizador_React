897f2698dd6d553f504bfd4cf27b9f193cfbfd4d
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-02-11 14:10:21 -0300

    7-87 primeros Styled Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import  Header from './components/Header';
+import styled from '@emotion/styled';
+
+
+// Para Centrar el Condenido
+const Contenedor = styled.div`
+
+  max-width: 600px;
+  margin:0 auto;
+
+`
+// Contenedor Para Formulario
+const ContenedorFormulario = styled.div`
+background-color:#FFF;
+padding: 3rem;
+`
 
 function App() {
 return(
-  <Header
 
-    titulo='Cotizador De Seguros'
+  <Contenedor>
+        <Header
+      titulo='Cotizador De Seguros'
 
-  />
+      />
+
+      <ContenedorFormulario>
+
+        
+      </ContenedorFormulario>
+  </Contenedor>
+  
+
+
 );
 
 }
